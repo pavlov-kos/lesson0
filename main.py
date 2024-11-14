950bf0f9fe339c722cdd2a14e37f5d4f241c98aa
pavlov-kos <kostiy2424@gmail.com>
2024-11-14 20:44:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#"1st program"
+print(12+89-1)
+print(9**0.5*5)
+#"2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+#"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+#"4th program"
+print('123.456')
+print(123.456)
+print(123.456*10)
+print(int(123.456%1*10))
